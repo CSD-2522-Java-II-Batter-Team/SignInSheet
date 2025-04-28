7795511045a565ffa068ac62293bbaf72f94faeb
0274074 <142900020+0274074@users.noreply.github.com>
2025-04-28 10:22:01 -0400

    Update SignInSheet.java
    Whitney S. was addedv to the nameList array

--- SignInSheet.java
+++ SignInSheet.java
@@ -4,7 +4,7 @@
  * File Name: SignInSheet.java
  * Last Update: 4/22/25 by Lillian H.
  * Last Update: 4/22/25 by James F.
- * Program Description: Program allows a user to enter their name into an array and prints out a confirmation message when ran.
+ * Program Description: The Program allows  user to enter their name into an array and prints out a confirmation message when run.
  */
 
 /*
@@ -14,7 +14,7 @@ Lillian H. - 4/22/25 - Added Lillian as the second element in the nameList array
 James F. - 4/22/25 - Added James as the third element in the nameList array.
 Tiffany W. - 4/22/2025 - Added Tiffany as the fourth element in the nameList array.
 Timothy G. - 4/26/2025 - Added Timothy as the fifth element in the nameList array.
-
+Whitney S. - 4/28/2025 - Added Whitney as the sixth element in the nameList array.
 ======================================
  */
 
@@ -25,15 +25,15 @@ Timothy G. - 4/26/2025 - Added Timothy as the fifth element in the nameList arra
         // Array to store our names
         String[] nameList = new String[6];
         
-        // Enter your name in the below list on the first available index you see. See my name for an example.
+        // Enter your name in the list below on the first available index you see. See my name for an example.
         nameList[0] = "Seth";
         nameList[1] = "Lillian";
         nameList[2] = "James";
         nameList[3] = "Tiffany";
         nameList[4] = "Timothy";
-        nameList[5] = "_______";
+        nameList[5] = "Whitney";
         
-        // Prints out statement showing you've modified the file.
+        // Prints out a statement showing you've modified the file.
         System.out.println();
         for (int i = 0; i <= 5; i++) {
             if (!nameList[i].isEmpty()) System.out.println("Hello, I'm " + nameList[i] + " and I'm able to modify the code!");
